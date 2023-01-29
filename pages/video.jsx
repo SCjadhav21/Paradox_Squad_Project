fe11efac9a71348b87033b73345f9fb394fa353d
Suraj Jadhav <jadhavsuraj4567@gmail.com>
2023-01-29 12:12:20 +0530

    Adding routes in backend

--- pages/video.jsx
+++ pages/video.jsx
@@ -4,9 +4,9 @@ import Head from "next/head";
 
 import { Box, Heading, Image, Text, Button } from "@chakra-ui/react";
 
-const video = () => {
+const video = ({ video }) => {
   const router = useRouter();
-  // console.log(video);
+  console.log(video, video.src);
 
   return (
     <>
@@ -23,11 +23,11 @@ const video = () => {
             <iframe
               width="560"
               height="315"
-              src="https://www.youtube.com/embed/T94PHkuydcw"
+              src="https://www.youtube.com/embed/73CoPPm9hjk"
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen="true"
+              allowFullScreen={true}
             ></iframe>
           </div>
         </Box>
@@ -35,28 +35,32 @@ const video = () => {
     </>
   );
 };
-// export async function getStaticPaths(){
-//   let responce = await fetch("http://localhost:8080/movies")
-//   let data = await responce.json()
-//   return{
-//     paths:data.map((el)=>({
-//       params:{id:el.id.toString()}
-//     })),
-//     fallback:false
-//   }
-// }
-
-// export async function getStaticProps(context){
-// const {params:{id}} = context
-//   let responce = await fetch(`http://localhost:8080/movies/${id}`)
-//   let data = await responce.json()
-
+// export async function getStaticPaths() {
+//   let responce = await fetch("http://localhost:4500/banking");
+//   let data = await responce.json();
 //   return {
-//     props: {
-//       video: data,
-//     },
+//     paths: data.map((el) => ({
+//       params: { id: el._id.toString() },
+//     })),
+//     fallback: false,
 //   };
 // }
+
+export async function getStaticProps(context) {
+  const {
+    // params: { id },
+  } = context;
+  let responce = await fetch(
+    `http://localhost:4500/banking/63d510bbf70ede2003627729`
+  );
+  let data = await responce.json();
+
+  return {
+    props: {
+      video: data,
+    },
+  };
+}
 
 // export async function getStaticProps(context) {
 //   const {
